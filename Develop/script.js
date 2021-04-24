d66ac6076bd73a86a3b9b955704ecb019488302b
michaelzadra27 <michaelzadra27@gmail.com>
2021-04-23 18:35:27 -0700

    cleaning up HTML, adding classed, got value back of current and schedule time in JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,61 @@
 var timeDisplayEl = $('#time-display');
-const currentTime =
+
+//const currentTime = 
+var RowEl = document.getElementById('addColornow')
+var appTime= document.querySelector('#getTime');
+var appValue= appTime.dataset.number
+var rightNow = moment().format('h')
+var scheduleTime = 
+
+
+console.log("+++++++++")
+console.log(RowEl)
+console.log("+++++++++")
+
+console.log(appTime)
+console.log(appValue)
+
+console.log(rightNow)
+
+ function addPast (){
+     RowEl.classList.add('past')
+
+ }
+
+function timeCheck(){
+ if (appValue < rightNow){
+    addPast();
+    console.log("&&&&&&&&&&")
+ }
+ else if (appValue === rightNow){
+     console.log()
+ }
+}
+
+ setInterval(function() {
+     var currentTime =moment();
+      $("#time-display").html(moment().format(`[Currrent Time and Date: <br>] dddd, MMMM Do YYYY, h:mm:ss a`));
+    
+      return currentTime
+  }, 1000)
+ 
+  //addColor();
+  timeCheck();
+  console.log("+++++++++++")
+  //console.log(currentTime)
+  console.log("+++++++++++")
+
+
+
+//Call current
+//Use JQuery to get div
+//creat variable in for loop for div
+//For Loop to loop over div
+//compare to current time
+//save time from app in one variable
+//save actual time in different variable
+//create if, else if, else
+//Based on time add/remove class and use for loop to touch each one
 
 
 
@@ -7,10 +63,5 @@ const currentTime =
 
 
 
-setInterval(function() {
-    var currentTime =moment();
-     $("#time-display").html(moment().format(`[Currrent Time and Date: <br>] dddd, MMMM Do YYYY, h:mm:ss a`));
-     return currentTime
- }, 1000);
 
- console.log(currentTime)
+ //console.log(currentTime)
